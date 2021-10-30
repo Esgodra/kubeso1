0bf52362406f43e8a88bb54ce8dd7da090e17b0b
Esgodra <esgodra@esgodra.localdomain>
2021-10-30 09:51:04 -0600

    Added kubernetes files

--- db/mysql-init.sql
+++ db/mysql-init.sql
@@ -16,14 +16,14 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 
-INSERT INTO `user` (id, INS_DATE, NAME, VALUE)
+INSERT INTO `user` (ID, CANT_POKE_COINS, USER, ADDRES, AUDIT_RECORD)
 VALUES
-( 1, 200, 'xXJoshueXx', 'Algun lugar de nuestra bella guatemala'),
-( 2, 500, 'Manuelito2021', 'Algun lugar de nuestra bella guatemala'),
-( 3, 6000, 'Carlos_Gaming777', 'Algun lugar de nuestra bella guatemala'),
-( 4, 200, 'Golosa69', 'Algun lugar de nuestra bella guatemala'),
-( 5, 200, 'Yanclov', 'Algun lugar de nuestra bella guatemala'),
-( 6, 300, 'Sputnik-V', 'Algun lugar de nuestra bella guatemala'),
-( 7, 400, 'Covidiano', 'Aqui nomas'),
-( 8, 600, 'Cachisumbado23', 'Algun lugar de nuestra bella guatemala'),
-( 9, 700, 'Chepito99', 'Algun lugar de nuestra bella guatemala');
+( 1, 200, 'xXJoshueXx', 'Algun lugar de nuestra bella guatemala',curdate()),
+( 2, 500, 'Manuelito2021', 'Algun lugar de nuestra bella guatemala',curdate()),
+( 3, 6000, 'Carlos_Gaming777', 'Algun lugar de nuestra bella guatemala',curdate()),
+( 4, 200, 'Golosa69', 'Algun lugar de nuestra bella guatemala',curdate()),
+( 5, 200, 'Yanclov', 'Algun lugar de nuestra bella guatemala',curdate()),
+( 6, 300, 'Sputnik-V', 'Algun lugar de nuestra bella guatemala',curdate()),
+( 7, 400, 'Covidiano', 'Aqui nomas',curdate()),
+( 8, 600, 'Cachisumbado23', 'Algun lugar de nuestra bella guatemala'.curdate()),
+( 9, 700, 'Chepito99', 'Algun lugar de nuestra bella guatemala',curdate());
